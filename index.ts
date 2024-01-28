d999279cfa23f90e8d9154c6919ab77d500ce900
yasasvi <yasasvigumma@gmail.com>
2024-01-28 07:40:36 +0530

    backend-api-is-done

--- index.ts
+++ index.ts
@@ -2,14 +2,18 @@ import {config} from "dotenv"
 import {OpenAI} from "openai"
 import {Elysia} from "elysia"
 config()
-async function Bot() {
+async function Bot(q:any) {
     const ai = new OpenAI({apiKey:process.env.API_KEY})
     const res = await ai.chat.completions.create({
         model:"gpt-3.5-turbo",
-        messages:[{role:"user",content:"What is the capital of india"}]
-    })
+        messages:[{role:"user",content:q}]
+    })  
     return res.choices[0].message.content
 }
 const app = new Elysia()
-    .get("/",() => "Hello World!")
+    .get("/:id",async ({params:{id}}:any) => {
+        const text = id.replace("-"," ")
+        const res = await Bot(text)
+        return res
+    })
     .listen(5000)
