bb97a725c6de34e7f45687c7172a99183d2f5ae2
yasasvi <yasasvigumma@gmail.com>
2024-01-28 20:25:32 +0530

    it is working but needs more tweaking... and neet to add speech to text

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import {config} from "dotenv"
 import {OpenAI} from "openai"
 import {Elysia} from "elysia"
+import {cors} from "@elysiajs/cors"
 config()
 async function Bot(q:any) {
     const ai = new OpenAI({apiKey:process.env.API_KEY})
@@ -11,6 +12,7 @@ async function Bot(q:any) {
     return res.choices[0].message.content
 }
 const app = new Elysia()
+    .use(cors())
     .get("/:id",async ({params:{id}}:any) => {
         const text = id.replace("-"," ")
         const res = await Bot(text)
--- script.js
+++
@@ -1,8 +0,0 @@
-// Speech to Text
-window.SpeechRecognition = window.webkitSpeechRecognition
-const recognition = new SpeechRecognition()
-
-// text to speech
-const uttrence = new SpeechSynthesisUtterance("Hello World")
-uttrence.rate = 1
-speechSynthesis.speak(uttrence)
