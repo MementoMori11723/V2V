d58a861bd834b61dd703388770e2fbfc886ca666
yasasvi <yasasvigumma@gmail.com>
2024-09-19 10:00:05 +0530

    Updated code
    changed data...

--- index.ts
+++ index.ts
@@ -1,21 +1,22 @@
-import {config} from "dotenv"
-import {OpenAI} from "openai"
-import {Elysia} from "elysia"
-import {cors} from "@elysiajs/cors"
-config()
-async function Bot(q:any) {
-    const ai = new OpenAI({apiKey:process.env.API_KEY})
-    const res = await ai.chat.completions.create({
-        model:"gpt-3.5-turbo",
-        messages:[{role:"user",content:q}]
-    })  
-    return res.choices[0].message.content
+import { config } from "dotenv";
+import { OpenAI } from "openai";
+import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+config();
+async function Bot(q: any) {
+  const ai = new OpenAI({ apiKey: process.env.API_KEY });
+  const res = await ai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: q }],
+  });
+  return res.choices[0].message.content;
 }
-const app = new Elysia()
-    .use(cors())
-    .get("/:id",async ({params:{id}}:any) => {
-        const text = id.replace("-"," ")
-        const res = await Bot(text)
-        return res
-    })
-    .listen(5000)
+const app = new Elysia();
+app
+  .use(cors())
+  .get("/:id", async ({ params: { id } }: any) => {
+    const text = id.replace("-", " ");
+    const res = await Bot(text);
+    return res;
+  })
+  .listen(5000);
