f893166605af8276ed7aa6f86311cde9b748d9d8
yasasvi <yasasvigumma@gmail.com>
2024-10-25 22:03:26 +0530

    Using TTS structure...
    this allows us to send our data properly...

--- server/apis/TTS.go
+++ server/apis/TTS.go
@@ -2,12 +2,10 @@ package apis
 
 import (
 	"V2V/config"
-	"errors"
 )
 
 type AudioConfig struct {
 	AudioEncoding    string   `json:"audioEncoding"`
-	EffectsProfileId []string `json:"effectsProfileId"`
 	Pitch            float64  `json:"pitch"`
 	SpeakingRate     float64  `json:"speakingRate"`
 }
@@ -27,10 +25,28 @@ type TTSResponse struct {
 	Voice       Voice       `json:"voice"`
 }
 
+func PostRequest(URL string, data interface{}, API_KEY string) ([]byte, error) {
+  return nil, nil
+}
+
 func GetTTSResponse(data string) ([]byte, error) {
 	API_KEY, URL := config.GetTtsApiDetails()
-	if URL != "" && API_KEY != "" {
-		return nil, errors.New("TTS API details not found")
-	}
-	return nil, nil
+  Responce := TTSResponse{
+    AudioConfig: AudioConfig{
+      AudioEncoding: "LINEAR16",
+      Pitch: 0,
+      SpeakingRate: 1,
+    },
+    Input: Input{
+      Text: data,
+    },
+    Voice: Voice{
+      LanguageCode: "en-US",
+      Name: "en-US-Standard-G",
+    },
+  }
+  return PostRequest(
+    URL, Responce, 
+    API_KEY,
+  )
 }
