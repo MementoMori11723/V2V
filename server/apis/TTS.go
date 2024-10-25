05f16cd7373a274e0ed1c25d580feff088931327
yasasvi <yasasvigumma@gmail.com>
2024-10-25 18:13:54 +0530

    Add TTS structure
    this will help us encode audio file...

--- server/apis/TTS.go
+++ server/apis/TTS.go
@@ -5,10 +5,32 @@ import (
 	"errors"
 )
 
+type AudioConfig struct {
+	AudioEncoding    string   `json:"audioEncoding"`
+	EffectsProfileId []string `json:"effectsProfileId"`
+	Pitch            float64  `json:"pitch"`
+	SpeakingRate     float64  `json:"speakingRate"`
+}
+
+type Input struct {
+	Text string `json:"text"`
+}
+
+type Voice struct {
+	LanguageCode string `json:"languageCode"`
+	Name         string `json:"name"`
+}
+
+type TTSResponse struct {
+	AudioConfig AudioConfig `json:"audioConfig"`
+	Input       Input       `json:"input"`
+	Voice       Voice       `json:"voice"`
+}
+
 func GetTTSResponse(data string) ([]byte, error) {
-  API_KEY, URL := config.GetTtsApiDetails()
-  if URL != "" && API_KEY != "" {
-    return nil, errors.New("TTS API details not found")
-  }
-  return nil, nil
+	API_KEY, URL := config.GetTtsApiDetails()
+	if URL != "" && API_KEY != "" {
+		return nil, errors.New("TTS API details not found")
+	}
+	return nil, nil
 }
